38204e9af06c81d301757cb4d711a001befc166a
terryfeverpitch <terryfeverpitch@gmail.com>
2014-12-02 07:57:47 +0800

    upload map

---
+++ src/com/simple/map/MapActivity.java
@@ -0,0 +1,88 @@
+package com.simple.map;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MapActivity extends Activity {
+	private MapView map_mv_map;
+	private Button map_btn_search;
+	
+	public static ProgressDialog progressdialog;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map);
+		progressdialog = new ProgressDialog(this);
+		progressdialog.setTitle("Loading");
+		progressdialog.setMessage("map is loading");
+		progressdialog.show();
+		
+		map_mv_map = (MapView) findViewById(R.id.map_mv_map);
+		map_btn_search = (Button) findViewById(R.id.map_btn_search);
+		map_btn_search.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if(map_mv_map.isReady())
+					map_mv_map.gotoMap();
+			}
+		});
+		// LatLng(25.074389, 121.661089) -> TTU
+		
+		//map_mv_map.loadUrl("javascript:initmap('"+ latitude + "','" + longitude+"')");
+//		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, MapActivity.this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.map, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+/*	// for GPS update
+	@Override
+	public void onLocationChanged(Location location) {
+		Double longitude = location.getLongitude() ;
+		Double latitude = location.getLatitude() ;
+		locationManager.removeUpdates(MapActivity.this);
+		map_wv_map.loadUrl("javascript:initmap('"+ latitude + "','"+longitude+"')");
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+*/
+}
---
+++ src/com/simple/map/MapView.java
@@ -0,0 +1,55 @@
+package com.simple.map;
+
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MapView extends WebView {
+	private String MAP_URL = "file:///android_asset/map.html";
+	private boolean loaded = false;
+	private WebViewClient client = new WebViewClient() {
+		@Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {          
+			MapActivity.progressdialog.setTitle("Loading");
+			MapActivity.progressdialog.setMessage(url + "is loading");
+			MapActivity.progressdialog.show();    
+        }
+		
+		@Override
+	    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+	        return shouldOverrideUrlLoading(view, url);
+	    }
+		
+		@Override  
+		public void onPageFinished(WebView view, String url) {
+			MapView.this.loaded = true;
+			initMap();
+			MapActivity.progressdialog.dismiss();
+		}
+	};
+	
+	public MapView(final Context context, final AttributeSet attrs) {
+		super(context, attrs);
+		getSettings().setJavaScriptEnabled(true);
+		setWebViewClient(client);
+		loadUrl(MAP_URL);
+	}
+	
+	public void initMap() {
+		// 25.074378, 121.661085 = home
+		// 25.073260, 121.663002 = 八方雲集
+		loadUrl("javascript:initMap(25.074378, 121.661085)");
+	}
+	
+	public void gotoMap() {
+		String str = "樂凡手感烘焙";
+		loadUrl("javascript:goto('"+ str + "')");
+	}
+
+	public boolean isReady() {
+		return this.loaded;
+	}
+}
